cbdeda4c4757dce55383697c5f34b029f0c9b796
Maksym Zhuk <bitmaksum550@gmail.com>
2024-07-13 16:30:54 +0300

    add homePage

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-// $(`#coffeeMachine_5000`).click(() => {
-//     $(`#coffeeMachine_5000`).removeClass(`coffeeMachine_5000_box`).addClass(`coffeeMachine_5000_box_center`);
-//     $(`#coffeeMachine_1500`).removeClass(`coffeeMachine_1500_box_center`).addClass(`coffeeMachine_1500_box`);
-//     $(`#coffeeMachine_3000`).removeClass(`coffeeMachine_3000_box_center`).addClass(`coffeeMachine_3000_box`);
-// });
-
-// $(`#coffeeMachine_1500`).click(() => {
-//     $(`#coffeeMachine_1500`).removeClass(`coffeeMachine_1500_box`).addClass(`coffeeMachine_1500_box_center`);
-//     $(`#coffeeMachine_5000`).removeClass(`coffeeMachine_5000_box_center`).addClass(`coffeeMachine_5000_box`);
-//     $(`#coffeeMachine_3000`).removeClass(`coffeeMachine_3000_box_center`).addClass(`coffeeMachine_3000_box`);
-// });
-
-// $(`#coffeeMachine_3000`).click(() => {
-//     $(`#coffeeMachine_3000`).removeClass(`coffeeMachine_3000_box`).addClass(`coffeeMachine_3000_box_center`);
-//     $(`#coffeeMachine_1500`).removeClass(`coffeeMachine_1500_box_center`).addClass(`coffeeMachine_1500_box`);
-//     $(`#coffeeMachine_5000`).removeClass(`coffeeMachine_5000_box_center`).addClass(`coffeeMachine_5000_box`);
-// });
-
-
 $(`#coffeeMachine_5000`).click(() => {
     $(`#coffeeMachine_5000`).removeClass(`coffeeMachine_5000_box_left`).removeClass(`coffeeMachine_5000_box_right`).addClass(`coffeeMachine_5000_box_center`);
     $(`#coffeeMachine_1500`).removeClass(`coffeeMachine_1500_box_center`).removeClass(`coffeeMachine_1500_box_left`).addClass(`coffeeMachine_1500_box_right`);
@@ -33,4 +14,19 @@ $(`#coffeeMachine_1500`).click(() => {
     $(`#coffeeMachine_3000`).removeClass(`coffeeMachine_3000_box_center`).removeClass(`coffeeMachine_3000_box_left`).addClass(`coffeeMachine_3000_box_right`);
     $(`#coffeeMachine_1500`).removeClass(`coffeeMachine_1500_box_left`).removeClass(`coffeeMachine_1500_box_right`).addClass(`coffeeMachine_1500_box_center`);
     $(`#coffeeMachine_5000`).removeClass(`coffeeMachine_5000_box_center`).removeClass(`coffeeMachine_5000_box_right`).addClass(`coffeeMachine_5000_box_left`);
+});
+
+$(`#coffeeMachine_5000`).dblclick(()=>{
+    $(`.stratPage`).css(`display`, `none`);
+    $(`.homePage`).css(`display`, `flex`);
+});
+
+$(`#coffeeMachine_1500`).dblclick(()=>{
+    $(`.stratPage`).css(`display`, `none`);
+    $(`.homePage`).css(`display`, `flex`);
+});
+
+$(`#coffeeMachine_3000`).dblclick(()=>{
+    $(`.stratPage`).css(`display`, `none`);
+    $(`.homePage`).css(`display`, `flex`);
 });
