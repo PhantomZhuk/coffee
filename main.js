2583427844e755005ccfa543d6bafc890aebc949
Maksym Zhuk <bitmaksum550@gmail.com>
2024-07-14 19:51:24 +0300

    add incomplete tracking of power outages

--- main.js
+++ main.js
@@ -16,17 +16,116 @@ $(`#coffeeMachine_1500`).click(() => {
     $(`#coffeeMachine_5000`).removeClass(`coffeeMachine_5000_box_center`).removeClass(`coffeeMachine_5000_box_right`).addClass(`coffeeMachine_5000_box_left`);
 });
 
-$(`#coffeeMachine_5000`).dblclick(()=>{
+$(`#coffeeMachine_5000`).dblclick(() => {
     $(`.stratPage`).css(`display`, `none`);
     $(`.homePage`).css(`display`, `flex`);
 });
 
-$(`#coffeeMachine_1500`).dblclick(()=>{
+$(`#coffeeMachine_1500`).dblclick(() => {
     $(`.stratPage`).css(`display`, `none`);
     $(`.homePage`).css(`display`, `flex`);
 });
 
-$(`#coffeeMachine_3000`).dblclick(()=>{
+$(`#coffeeMachine_3000`).dblclick(() => {
     $(`.stratPage`).css(`display`, `none`);
     $(`.homePage`).css(`display`, `flex`);
 });
+
+let electricity = `gridPower`;//powerPlantElectricity
+let date = new Date();
+let presentDay = date.getDay();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+
+if (presentDay == 0) {
+    if (hours > 0 && hours < 6 || hours > 12 && hours < 16 || hours > 20 && hours < 22) {
+        electricity = `powerPlantElectricity`;
+    }else {
+        electricity = `gridPower`
+    }
+}else if (presentDay == 1){
+    if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+        electricity = `powerPlantElectricity`;
+    }else {
+        electricity = `gridPower`
+    }
+}
+// else if (presentDay == 2){
+    //     if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+    //         electricity = `powerPlantElectricity`;
+    //     }else {
+    //         electricity = `gridPower`
+    //     }
+    // }else if (presentDay == 3){
+    //     if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+    //         electricity = `powerPlantElectricity`;
+    //     }else {
+    //         electricity = `gridPower`
+    //     }
+    // }else if (presentDay == 4){
+    //     if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+    //         electricity = `powerPlantElectricity`;
+    //     }else {
+    //         electricity = `gridPower`
+    //     }
+    // }else if (presentDay == 5){
+    //     if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+    //         electricity = `powerPlantElectricity`;
+    //     }else {
+    //         electricity = `gridPower`
+    //     }
+    // }else if (presentDay == 6){
+    //     if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+    //         electricity = `powerPlantElectricity`;
+    //     }else {
+    //         electricity = `gridPower`
+    //     }
+    // }
+
+
+setInterval(() => {
+    if (presentDay == 0) {
+        if (hours > 0 && hours < 6 || hours > 12 && hours < 16 || hours > 20 && hours < 22) {
+            electricity = `powerPlantElectricity`;
+        }else {
+            electricity = `gridPower`
+        }
+    }else if (presentDay == 1){
+        if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+            electricity = `powerPlantElectricity`;
+        }else {
+            electricity = `gridPower`
+        }
+    }
+    // else if (presentDay == 2){
+    //     if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+    //         electricity = `powerPlantElectricity`;
+    //     }else {
+    //         electricity = `gridPower`
+    //     }
+    // }else if (presentDay == 3){
+    //     if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+    //         electricity = `powerPlantElectricity`;
+    //     }else {
+    //         electricity = `gridPower`
+    //     }
+    // }else if (presentDay == 4){
+    //     if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+    //         electricity = `powerPlantElectricity`;
+    //     }else {
+    //         electricity = `gridPower`
+    //     }
+    // }else if (presentDay == 5){
+    //     if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+    //         electricity = `powerPlantElectricity`;
+    //     }else {
+    //         electricity = `gridPower`
+    //     }
+    // }else if (presentDay == 6){
+    //     if (hours > 1 && hours < 4 || hours > 7 && hours < 10 || hours > 13 && hours < 17  || hours > 19 && hours < 23) {
+    //         electricity = `powerPlantElectricity`;
+    //     }else {
+    //         electricity = `gridPower`
+    //     }
+    // }
+}, 60000);
